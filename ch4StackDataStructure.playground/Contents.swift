9ec0391d8bdcea8895c1d703f0f3eec18fa7cb31
Simran Rai <simranrai@Simrans-MacBook-Pro.local>
2020-11-27 16:26:16 -0600

    finished lecture notes for ch 4

--- ch4StackDataStructure.playground/Contents.swift
+++ ch4StackDataStructure.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+// Stack Data Structure
 
-var str = "Hello, playground"
+// push: adding an element to the top of the stack
+// pop: removing the top element of the stack
+
+example(of: "using a stack") {
+    var stack = Stack<Int>()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    
+    print(stack)
+
+    if let poppedElement = stack.pop() {
+        assert(4 == poppedElement)
+        print("Popped: \(poppedElement)")
+    }
+}
+
+example(of: "initializing a stack from an array") {
+    let array = ["A", "B", "C", "D"]
+    var stack = Stack(array)
+    print(stack)
+    stack.pop()
+}
+
