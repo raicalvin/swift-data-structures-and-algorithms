fe2bc4f77699a1f5d5aa25c7adbc0cf99711b093
Simran Rai <simranrai@Simrans-MacBook-Pro.local>
2020-11-26 18:23:23 -0600

    finished chapter 2 reading

--- ch2Complexity.playground/Contents.swift
+++ ch2Complexity.playground/Contents.swift
@@ -1,1 +1,43 @@
-var str = "Hello, Swift! :)"
+// Chapter 2: Complexity
+
+// TIME COMPLEXITY
+
+// Constant Time
+// Has the same running time regardless of the input
+// In this function, the length of the array doesn't matter since it only ever checks the first element
+// This is O(1) since it is constant time
+func checkFirst(names: [String]) {
+    if let first = names.first {
+        print(first)
+    } else {
+        print("no names")
+    }
+}
+
+// Linear Time
+// The time increases linearly as the data increases in size
+// In this function, the longer the array, the longer the time
+// This is O(n) since it is linear time
+func printNames(names: [String]) {
+    for name in names {
+        print(name)
+    }
+}
+
+// Quadratic Time
+// The time is proportional to the square of the input size
+// O(n^2) since every increase in data increase the time quadratically
+func printNames2(names: [String]) {
+    for _ in names {
+        for name in names {
+            print(name)
+        }
+    }
+}
+
+// Logarithmic Time
+// Quasilinear Time
+
+// SPACE COMPLEXITY
+
+
